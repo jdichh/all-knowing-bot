2b632fd2066d9e44869ae6f6a10a6489b95ca1d1
jdichh <jpsdichoso21@gmail.com>
2023-08-02 22:37:22 +0800

    input border color changes when btn is pressed

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,7 @@
-console.log("hello!")
+document.getElementById("send-it").addEventListener('mousedown', function () {
+    document.getElementById("user-input").style.borderTopColor = '#EEEEEE'
+})
+
+document.getElementById("send-it").addEventListener('mouseup', function () {
+    document.getElementById("user-input").style.borderTopColor = '#5B74E3'
+})
