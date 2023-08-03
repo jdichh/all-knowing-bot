afcb212180fe46c8edc432e96cf2e787463bfe9e
jdichh <jpsdichoso21@gmail.com>
2023-08-03 12:18:19 +0800

    adding the chat-gpt api & dom elements

--- javascript/index.js
+++ javascript/index.js
@@ -2,15 +2,83 @@ const chatBoxLog = document.getElementById("chatbox-prompts");
 const userInput = document.getElementById("user-input");
 const sendButton = document.getElementById("send-it");
 const sendButtonIcon = document.getElementById("send-it-icon");
-const apiKey = import.meta.env.VITE_API_KEY;
 
 sendButton.addEventListener("click", sendMessage);
 userInput.addEventListener("keydown", (e) => {
   if (e.key == "Enter") sendMessage();
 });
 
-function sendMessage() {
-    alert("Hello!")
-    console.log(apiKey)
+function appendMessage(sender, message) {
+  const messageDiv = document.createElement("div");
+  const iconDiv = document.createElement("div");
+  const icon = document.createElement("i");
+  const chatDiv = document.createElement("div");
+
+  chatDiv.classList.add("chatbox-prompts");
+  iconDiv.classList.add("icon");
+  messageDiv.classList.add(sender);
+  messageDiv.innerText = message;
+
+  if (sender == "user") {
+    icon.classList.add("fa-regular", "fa-user");
+    iconDiv.setAttribute("id", "user-icon");
+  } else {
+    icon.classList.add("fa-solid", "fa-robot");
+    iconDiv.setAttribute("id", "ai-icon");
+  }
+
+  iconDiv.appendChild(icon);
+  chatDiv.appendChild(iconDiv);
+  chatDiv.appendChild(messageDiv);
+  chatBoxLog.appendChild(chatDiv);
+  chatBoxLog.scrollTo = chatBoxLog.scrollHeight;
 }
 
+function sendMessage() {
+  const message = userInput.value.trim();
+  if (message == "") return;
+
+  appendMessage("user", message);
+  userInput.value = "";
+
+  const url ="https://chatgpt-chatgpt3-5-chatgpt4.p.rapidapi.com/v1/chat/completions";
+  const options = {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      "X-RapidAPI-Key": `${import.meta.env.VITE_API_KEY}`,
+      "X-RapidAPI-Host": "chatgpt-chatgpt3-5-chatgpt4.p.rapidapi.com",
+    },
+    body: `{
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        {
+          "role": "user",
+          "content": "${message}"
+        }
+      ],
+      "temperature": 0.8
+    }`,
+  };
+
+  fetch(url, options)
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      appendMessage("ai", response.choices[0].message.content);
+      sendButtonIcon.classList.add("fa-solid", "fa-paper-plane");
+      sendButtonIcon.classList.remove("fas", "fa-spinner", "fa-pulse");
+    })
+    .catch((err) => {
+      console.log(err);
+      if (err.name === "TypeError") {
+        appendMessage("ai", "Error: Check your API key if it is valid.");
+        sendButtonIcon.classList.add("fa-solid", "fa-paper-plane");
+        sendButtonIcon.classList.remove("fas", "fa-spinner", "fa-pulse");
+      }
+    });
+}
+
+// function testMessage() {
+//   console.log("hello!");
+// }
